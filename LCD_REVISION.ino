43f0ba1b06241f8385cc8e6a393de2f0d89c2388
Olabode Kayode <62682632+Olabodekayode@users.noreply.github.com>
2020-04-12 19:41:55 +0100

    Ultrasonic Sensor

--- LCD_REVISION.ino
+++ LCD_REVISION.ino
@@ -37,6 +37,66 @@ void loop ()
   Serial.print ("Target Distance: ");
   Serial.print (targetDistance);
   Serial.println (" m");
+  
+//   Another program
+  This program is for the use of ultrasonic sensor and servo 
+  
+#include <Servo.h>
+int TrigPin = 13;
+int EchoPin = 11;
+int ServoPin = 9;
+int redLed = 3;
+float servoAngle;
+
+float targetDistance;
+float pingTime;
+float SpeedOfSound = 776.7;
+Servo Mypointer;
+
+void setup ()
+{
+  Serial.begin (9600);
+  pinMode (TrigPin, OUTPUT);
+  pinMode (EchoPin, INPUT);
+  pinMode (ServoPin, OUTPUT);
+
+  Mypointer.attach (ServoPin);
+
+}
+
+void loop ()
+{
+  digitalWrite (TrigPin, LOW);
+  delayMicroseconds (2000);
+  digitalWrite (TrigPin, HIGH);
+  delayMicroseconds (15);
+  digitalWrite (TrigPin, LOW);
+
+  pingTime = pulseIn(EchoPin, HIGH);
+  pingTime = pingTime / 1000000;
+  pingTime = pingTime / 3600;
+
+  targetDistance = SpeedOfSound * pingTime;
+  targetDistance = targetDistance / 2;
+  targetDistance = targetDistance * 63360;
+
+  Serial.print ("Distance of the object ");
+  Serial.print (targetDistance);
+  Serial.println (" inches");
+
+  if (targetDistance > 4.0)
+  {
+    analogWrite (redLed, targetDistance );
+
+  }
+  else
+    analogWrite (redLed, LOW);
+
+
+  servoAngle = 106. / 7. * (targetDistance) + 37;
+  Mypointer.write(servoAngle);
+}
+  
 
   LCD.setCursor(0, 1);
   LCD.print ("                 ");
